f13f86d65d7b56aa5e2e061960c64988af6b7f10
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 10:33:51 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@226db48

--- src/sleep/index.ts
+++ src/sleep/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/time/0.12.0/docs/resources/sleep
 // generated from terraform resource schema
 
