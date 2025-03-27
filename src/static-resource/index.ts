ca24f1a523e171e6f8848f9087847803a219ba99
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-27 22:19:31 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2db01f0

--- src/static-resource/index.ts
+++ src/static-resource/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/time/0.13.0/docs/resources/static
 // generated from terraform resource schema
 
